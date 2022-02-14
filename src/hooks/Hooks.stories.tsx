117631ba309813fb83cf3e214604f88a7726dcc9
Gary Mayfield <gary.mayfield@hotmail.co.uk>
2022-02-14 09:05:57 +0000

    Added story for useFetch hook

---
+++ src/hooks/Hooks.stories.tsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import './Hooks.scss';
+
+export default {
+    title: 'ComponentLibrary/Hook Library',
+};
+
+export const UseFetch = () => (
+    <div className="hooks">
+        <h1>UseFetch</h1>
+        <figure>
+            <figcaption>useFetch.ts</figcaption>
+            <pre>
+                <code className="language-css">{`
+                    import { useState, useEffect, useRef } from "react";
+
+                    interface Cache<T> {
+                    [url: string]: T;
+                    }
+
+                    export default function useFetch<T>(url: string, init: RequestInit): [T, boolean] {
+                    const cache = useRef<Cache<T>>({});
+                    const [data, setData] = useState<T>();
+                    const [loading, setLoading] = useState(true);
+
+                    async function FetchURL() {
+                        if (cache.current[url]) {
+                        const data = cache.current[url];
+                        setData(data);
+                        } else {
+                        await fetch(url, init)
+                            .then((res) => {
+                            return res.status === 404 || res.status === 400 ? null : res.json();
+                            })
+                            .then((res) => {
+                            setData(res);
+                            setLoading(false);
+                            cache.current[url] = res;
+                            })
+                            .catch((error) => {
+                            console.error("Fetch to api failed", error);
+                            });
+                        }
+                    }
+                    useEffect(() => {
+                        FetchURL();
+                    }, [url]);
+
+                    return [data as T, loading];
+                    }
+            
+          `}</code>
+            </pre>
+        </figure>
+    </div>
+);
