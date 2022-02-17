fd6d6a86b09814cadfa69c310c791dffb2427cdf
Gary Mayfield <gary.mayfield@hotmail.co.uk>
2022-02-17 10:01:24 +0000

    Added story for new hook useMedia.

--- src/hooks/Hooks.stories.tsx
+++ src/hooks/Hooks.stories.tsx
@@ -78,3 +78,37 @@ export const UseToggle = () => (
         </figure>
     </div>
 );
+
+export const UseMedia = () => (
+    <div className="hooks">
+        <h1>UseMedia</h1>
+        <figure>
+            <figcaption>useMedia.ts</figcaption>
+            <pre>
+                <code className="language-css">{`
+                    import { useEffect, useState } from "react";
+
+                    export default function useMedia(queries: string[], values: number[], defaultValue: number){
+                        const mediaQueryLists = queries.map((q) => window.matchMedia(q));
+
+                        const getValue = () => {
+                            const index = mediaQueryLists.findIndex((mql) => mql.matches);
+                            return typeof values[index] !== 'undefined' ? values[index] : defaultValue;
+                        };
+
+                        const [value, setValue] = useState(getValue);
+
+                        useEffect(() => {
+                            const handler = () => setValue(getValue);
+                            mediaQueryLists.forEach((mql) => mql.addEventListener('change', handler));
+
+                            return () => mediaQueryLists.forEach((mql) => mql.removeEventListener('change', handler));
+                        }, []);
+
+                        return value;
+                    }
+          `}</code>
+            </pre>
+        </figure>
+    </div>
+);
